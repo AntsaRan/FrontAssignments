eccc3e147b7ee3568deabf69c08388bd0de1b7d2
Antsa <vranari@yahoo.fr>
2021-04-08 20:28:40 +0300

    antsa

--- assignment-app2/src/app/app.component.ts
+++ assignment-app2/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AssignmentsService } from './shared/assignments.service';
 import { AuthService } from './shared/auth.service';
 
 @Component({
@@ -10,14 +11,22 @@ import { AuthService } from './shared/auth.service';
 export class AppComponent {
   title = 'Application de gestion d\'assignments';
 
-  constructor(private authService:AuthService,private router:Router){}
+  constructor(private authService: AuthService, private router: Router, private assignmentsService: AssignmentsService) { }
 
-  login(){
-    if(this.authService.loggedIn){
+  login() {
+    if (this.authService.loggedIn) {
       this.authService.logOut();
       this.router.navigate(["/home"]);
-    }else{
-      this.authService.logIn("admin","toto");
+    } else {
+      this.authService.logIn("admin", "toto");
     }
+  }
+  peuplerbase() {
+    //this.assignmentsService.peuplerBD(); 
+    this.assignmentsService.peuplerBDavecForkJoin()
+      .subscribe(() => {
+        console.log("la bd a été peuplée");
+        this.router.navigate(["/home"],{replaceUrl:true});
+      })
   }
 }
--- assignment-app2/src/app/shared/assignments.service.ts
+++ assignment-app2/src/app/shared/assignments.service.ts
@@ -1,9 +1,11 @@
 import { ThrowStmt } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { Assignment } from '../assignments/assignment.model';
-import { Observable, of } from 'rxjs';
+import { forkJoin, Observable, of } from 'rxjs';
+import { catchError, filter, map } from 'rxjs/operators';
 import { LoggingService } from './logging.service';
 import { HttpClient,HttpHeaders  } from '@angular/common/http';
+import { bdInitialAssignments } from './data/data';
 
 @Injectable({
   providedIn: 'root'
@@ -11,25 +13,81 @@ import { HttpClient,HttpHeaders  } from '@angular/common/http';
 export class AssignmentsService {
   assignments: Assignment[];
   constructor(private loggingservice:LoggingService,private http:HttpClient) { } 
-  uri = "https://api-assingments.herokuapp.com/api/assignments";
+  uri = "http://localhost:8010/api/assignments";
 
   getAssignments():Observable<Assignment[]>{
     return this.http.get<Assignment[]>(this.uri);
   }
 
+  getAssignmentsPagine(page:number,limit:number):Observable<any>{
+    return this.http.get<Assignment[]>(this.uri+"?page="+page+"&limit="+limit);
+  }
+  peuplerBD() {
+    let i =1;
+    bdInitialAssignments.forEach(a => {
+      
+      let nouvelAssignment = new Assignment();
+      nouvelAssignment.nom = a.nom;
+      nouvelAssignment.id = a.id;
+      nouvelAssignment.dateRendu = new Date(a.dateRendu);
+      nouvelAssignment.rendu = a.rendu;
+      console.log(i+" nouvelAssignment.dateRendu");
+      this.addAssignment(nouvelAssignment)
+      .subscribe(reponse => {
+        //console.log(reponse.message);
+        i++;
+      })
+    })
+  }
+
+  peuplerBDavecForkJoin():Observable<any> {
+    const appelsversAddAssignment = [];
+
+    bdInitialAssignments.forEach(a => {
+      let nouvelAssignment = new Assignment();
+      nouvelAssignment.nom = a.nom;
+      nouvelAssignment.id = a.id;
+      nouvelAssignment.dateRendu = new Date(a.dateRendu);
+      nouvelAssignment.rendu = a.rendu;
+
+      appelsversAddAssignment.push(this.addAssignment(nouvelAssignment));
+    });
+    return forkJoin(appelsversAddAssignment);
+  }
+  
+  
   getAssignment(id:number): Observable<Assignment> {
    /* console.log(id + "id serv")
     let assignmentSearch = this.assignments.find(a =>a.id===id);
     console.log(assignmentSearch.nom);
     return of(assignmentSearch);*/
-    return this.http.get<Assignment>(this.uri+"/"+id);
+    return this.http.get<Assignment>(this.uri+"/"+id)
+    // pipe modifie la donnée reçu avant de la renvoyer
+    .pipe(
+      map(a => {
+        a.nom += " Modifié par map";
+        return a;
+      }),
+      catchError(this.handleError<any>('### catchError: getAssignmentByID avec id='+id))
+      );
+      /*,
+      filtrer les données avant de les envoyer
+      filter(a => {
+        return (a.rendu)
+      })*/
+    }
 
+    
+  private handleError<T>(operation: any, result?: T){
+    return (error:any): Observable<T> => {
+      console.log(error);
+      console.log(operation + ' a échoué' + error.message);
+      return of(result as T); 
+    }
   }
-  addAssignment(assignment:Assignment):Observable<Object>{
+  addAssignment(assignment:Assignment):Observable<any>{
     assignment.id=this.generateId();
-   /* this.assignments.push(assignment);*/
-   const headers = new HttpHeaders()
-   .set('Content-Type', 'application/json');
+  
     return this.http.post(this.uri,assignment);
   }
 
@@ -37,12 +95,12 @@ export class AssignmentsService {
   return Math.round(Math.random()*100000);  
   }
 
-  updateAssignment(assignment:Assignment):Observable<Object>{
+  updateAssignment(assignment:Assignment):Observable<any>{
     return this.http.put(this.uri,assignment);  
   }
 
-  deleteAssignment(assignment:Assignment):Observable<Object>{
-    return this.http.delete(this.uri+"/"+assignment.id);
-}
+  deleteAssignment(assignment:Assignment):Observable<any>{
+    return this.http.delete(this.uri+"/"+assignment._id);
+  }
 }
  
